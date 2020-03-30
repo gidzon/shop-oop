e9149e11dd32cacf7e6fcce82aa023f09e501bfa
Nicolay Vorobev <sumakaki37rus@gmail.com>
2020-03-30 15:44:11 +0300

    изменил верстку

--- index.php
+++ index.php
@@ -16,21 +16,27 @@ $categories = Category::getCategories($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <header>
-<?php if(!empty($_SESSION['auth'])): ?>
-        <a href="profile.php?id=<?php echo $data['id']; ?>"><?php echo $_SESSION['auth']; ?></a>
-        <a href="handler/exit.php">Выход</a>
-        <a href="admin.php">admin</a>
-    <?php else: ?>
-        <a href="reg.php">Регистрация</a>
-        <a href="auth.php">Авторизация</a>
-    <?php endif; ?>
+    <div class="link">
+        <?php if(!empty($_SESSION['auth'])): ?>
+                <a href="profile.php?id=<?php echo $data['id']; ?>"><?php echo $_SESSION['auth']; ?></a>
+                <a href="handler/exit.php">Выход</a>
+                <a href="admin.php">admin</a>
+            <?php else: ?>
+                <a href="reg.php">Регистрация</a>
+                <a href="auth.php">Авторизация</a>
+        <?php endif; ?>
+    </div>
 </header>
 <body>
     <?php foreach($categories as $category): ?>
     <div class="category">
-        <a href="products.php?idCategory=<?php echo $category['idcategory']; ?>"><H1><?php echo $category['category']; ?></H1></a>
+        <div class="title">
+            <a  href="products.php?idCategory=<?php echo $category['idcategory']; ?>"><H1><?php echo $category['category']; ?></H1></a>
+        </div>
+        
         <img src="<?php echo $category['image']; ?>" alt="">
     </div>
     <?php endforeach; ?>
--- products.php
+++ products.php
@@ -16,25 +16,30 @@ $data = $products->getProductsFromCategory($pdo, $_GET['idCategory']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
 <header>
-    <?php if(!empty($_SESSION['auth'])): ?>
-        <a href="profile.php?id=<?php echo $val['id']; ?>"><?php echo $_SESSION['auth']; ?></a>
-        <a href="handler/exit.php">Выход</a>
-    <?php else: ?>
-        <a href="reg.php">Регистрация</a>
-        <a href="auth.php">Авторизация</a>
-    <?php endif; ?>
+    <div class="link">
+        <?php if(!empty($_SESSION['auth'])): ?>
+            <a href="profile.php?id=<?php echo $val['id']; ?>"><?php echo $_SESSION['auth']; ?></a>
+            <a href="handler/exit.php">Выход</a>
+        <?php else: ?>
+            <a href="reg.php">Регистрация</a>
+            <a href="auth.php">Авторизация</a>
+        <?php endif; ?>
+        <a href="/">home</a>
+    </div>
 </header>
-    <a href="/">home</a>
     <?php foreach($data as $product): ?>
     <div class="product">
-       <a href="product.php?idproduct=<?php echo $product['idproduct']; ?>"><H1><?php echo $product['title']; ?></H1></a>
+        <div class="title">
+            <a href="product.php?idproduct=<?php echo $product['idproduct']; ?>"><H1><?php echo $product['title']; ?></H1></a>
+        </div>
         <img src="<?php echo $product['image']; ?>" alt="">
-        <p><?php echo $product['price']; ?></p>
-        <a href="product.php?id=<?php echo $product['idproduct']; ?>">подробнее</a>
+        <p ><?php echo $product['price']; ?></p>
+        <a id="detailed" href="product.php?idproduct=<?php echo $product['idproduct']; ?>">подробнее</a>
     </div>
     <?php endforeach; ?>
 </body>
